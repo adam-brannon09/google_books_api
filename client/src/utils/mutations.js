f25a33e5c035a6369896a7838b6eb6ac1a7c3144
adambrannon09-jhKgEp <66850339+adam-brannon09@users.noreply.github.com>
2023-07-21 15:51:14 -0500

    added another mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,10 @@
 import { gql } from '@apollo/client';
 
 
+
+
+
+export const ADD_USER = gql`
 mutation Mutation($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
@@ -9,3 +13,6 @@ mutation Mutation($username: String!, $email: String!, $password: String!) {
       }
     }
   }
+  `;
+
+  
