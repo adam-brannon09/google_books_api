a85ddb1d434f91f2f053176c3202df36b4459f1b
Adam Brannon <adambrannon@Adams-MacBook-Pro.local>
2023-08-01 00:26:09 -0500

    build was unsuccessful due to <Switch/> in app.js, changed to <routes/>. commented out all files in the routes folder. app working

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
@@ -36,17 +36,17 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
-  </ApolloProvider>
+      <Router>
+        <>
+          <Navbar />
+            <Routes>
+              <Route path='/' element={<SearchBooks/>} />
+              <Route path='/saved' element={<SavedBooks/>} />
+              <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+            </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
